2b9f9170eb4ac61d8292553c3bcb722ba7c60edf
Saurabh77-99 <saurabhdavda7799@gmail.com>
2025-09-19 12:47:32 +0530

    error came while deploying!

--- app/admin/import/page.tsx
+++ app/admin/import/page.tsx
@@ -1,1 +1,11 @@
-// Excel import interface
+import React from 'react';
+
+// This is the default export that Next.js needs.
+export default function ImportPage() {
+  return (
+    <div>
+      <h1>Import Page</h1>
+      {/* Your page content will go here */}
+    </div>
+  );
+}
