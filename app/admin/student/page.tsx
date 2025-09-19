46fed8b4b3b398e494e95a741367098723c42f64
Saurabh77-99 <saurabhdavda7799@gmail.com>
2025-09-19 13:06:56 +0530

    error came while deploying!

--- app/admin/student/page.tsx
+++ app/admin/student/page.tsx
@@ -1,1 +1,10 @@
-// Student management
+import React from 'react';
+
+// This default export is required for any page file.
+export default function StudentPage() {
+  return (
+    <div>
+      <h1>Admin Dashboard</h1>
+    </div>
+  );
+}
